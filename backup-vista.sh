b5ed476492d7ff0e93de763ad12216fe8f3f6d43
Alexis Carlson <whatisthehumanspirit@gmail.com>
2015-10-27 21:14:07 +0000

    Initial code commit.

---
+++ backup-vista.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script will backup your OSEHRA VistA environment. It will delete any previous backup.
+
+# This step ensures .mje and .mjo files will be created in /home/osehra
+cd /home/osehra
+
+echo "Deleting VistA repositories. You can always git them back."
+rm -rf Dashboard/VistA
+rm -rf Dashboard/VistA-M
+
+echo "Backing up your OSEHRA VistA environment."
+
+echo "Backing up the GT.M database."
+rm backup/*
+mupip backup "*" backup/
+cp g/osehra.gld backup/
+
+echo "Killing all MUMPS processes."
+test_list=$(ps -aux | grep '[m]umps' | awk '{print $2}')
+for process in $test_list
+do
+  mupip stop $process
+done
+
+echo "Deleting any previous backup."
+rm -rf /tmp/osehra
+
+echo "Copying files."
+rsync -a --exclude "/.bash_history" --exclude="/g/*" --exclude="/.gitconfig" --exclude="/.ssh" --exclude "/.viminfo" --exclude "/vista-environment-scripts" /home/osehra/ /tmp/osehra
+
+echo "Restarting Taskman."
+mumps -dir <<< "S DUZ=1 D ^ZTMB H"
+
---
+++ restore-vista.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script will restore your OSEHRA VistA environment using the last backup. Make sure you have a backup.
+
+echo "Restoring your OSEHRA VistA environment using the last backup."
+
+echo "Killing all MUMPS processes."
+test_list=$(ps -aux | grep '[m]umps' | awk '{print $2}')
+for process in $test_list
+do
+  mupip stop $process
+done
+
+echo "Deleting the current VistA environment."
+shopt -s extglob
+rm -rf /home/osehra/!(.bash_history|.gitconfig|.ssh|.viminfo|vista-environment-scripts)
+shopt -u extglob
+
+echo "Copying files."
+rsync -a /tmp/osehra/ /home/osehra
+
+echo "Restoring the GT.M database."
+cd /home/osehra
+mv backup/* g/
+
+echo "Reloading environment variables."
+cd /home/osehra
+. ".profile"
+source etc/env
+
+echo "Restarting Taskman."
+mumps -dir <<< "S DUZ=1 D ^ZTMB H"
+
