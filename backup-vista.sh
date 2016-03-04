39901eab2e179e0b745e2dbf375cc058916e343d
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-03-04 00:52:23 -0800

    Restart Taskman after backing up.

--- backup-vista.sh
+++ backup-vista.sh
@@ -27,3 +27,6 @@ rsync -a --exclude "/.bash_history" --exclude="/g/*" --exclude="/.gitconfig" --e
 
 echo "Cleaning up."
 rm backup/*
+
+echo "Restarting Taskman."
+mumps -dir <<< "S DUZ=1 D ^ZTMB H"
